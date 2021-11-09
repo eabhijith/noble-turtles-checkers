2e9caba7c89dda6014268a445e29f5f4d15be8af
Abhijith <eabhijith@gmail.com>
2021-11-09 09:34:33 +0100

    #12 sample test

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('routes using array of routers ', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  //Implement Testing from here...
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
 });
